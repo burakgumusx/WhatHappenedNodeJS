b518cdb2e436b9b4e8d12d56650da10756b80ff5
DESKTOP-U223FLP\BURAK <burakgumusfb@gmail.com>
2017-01-01 22:40:46 +0300

    add try catch scope in userservice.js

--- WhatHappened/database/userservice.js
+++ WhatHappened/database/userservice.js
@@ -5,54 +5,73 @@ var jwt = require('jwt-simple');
 var global = require('../constants/constants');
 
 exports.GetUser = function (res) {
-    var data = [];
-    mongodb.connect(mongoUrl, function (err, db) {
-        db.collection('USERS').find().toArray(function (err, cursor) {
-            cursor.forEach(function (row) {
-                data.push(row);
+    try {
+
+
+        var data = [];
+        mongodb.connect(mongoUrl, function (err, db) {
+            db.collection('USERS').find().toArray(function (err, cursor) {
+                cursor.forEach(function (row) {
+                    data.push(row);
+                });
+                res.send(data, 200);
             });
-            res.send(data, 200);
+            db.close();
         });
-        db.close();
-    });
+    }
+    catch (err) {
+        res.send(global.SOMETHING_WENT_WRONG, 500)
+    }
 };
 
 exports.SaveUser = function (req, res) {
 
-    mongodb.connect(mongoUrl, function (err, db) {
-        db.collection('USERS', function (err, col) {
-            col.save({
-                USERNAME: req.body.USERNAME,
-                PASSWORD: req.body.PASSWORD,
-                ADDRESS: req.body.ADDRESS
-            }, function () {
+    try {
+        
+        mongodb.connect(mongoUrl, function (err, db) {
+            db.collection('USERS', function (err, col) {
+                col.save({
+                    USERNAME: req.body.USERNAME,
+                    PASSWORD: req.body.PASSWORD,
+                    ADDRESS: req.body.ADDRESS
+                }, function () {
+                });
+                SaveToken(req, res);
             });
-            SaveToken(req, res);
+            db.close();
         });
-        db.close();
-    });
+    }
+    catch (err) {
+        res.send(global.SOMETHING_WENT_WRONG, 500);
+    }
 
 }
 
 function SaveToken(req, res) {
-    var token = new Date().getTime();
-    token = jwt.encode({"token": token.toString()}, 'shh', 'HS256');
+    try {
 
-    mongodb.connect(mongoUrl, function (err, db) {
-        db.collection('AUTH', function (err, col) {
-            var date = new Date(Date.now()).addDays(1);
-            col.save({TOKEN: token, EXPIRE: date, IPADDRESS: req.connection.remoteAddress.toString()}, function () {
+
+        var token = new Date().getTime();
+        token = jwt.encode({"token": token.toString()}, 'shh', 'HS256');
+
+        mongodb.connect(mongoUrl, function (err, db) {
+            db.collection('AUTH', function (err, col) {
+                var date = new Date(Date.now()).addDays(1);
+                col.save({TOKEN: token, EXPIRE: date, IPADDRESS: req.connection.remoteAddress.toString()}, function () {
+                });
             });
+            db.close();
+            res.send(token, 200);
         });
-        db.close();
-        res.send(token, 200);
-    });
+    }
+    catch (err) {
+        res.send(global.SOMETHING_WENT_WRONG, 500);
+    }
 }
 
 exports.CheckToken = function (res, next, token) {
-
-    if (token) {
-        try {
+    try {
+        if (token) {
             mongodb.connect(mongoUrl, function (err, db) {
                 db.collection("AUTH").findOne({
                     "EXPIRE": {$gt: new Date(Date.now())},
@@ -67,13 +86,14 @@ exports.CheckToken = function (res, next, token) {
                 });
                 db.close()
             });
+
         }
-        catch (err) {
-            res.end(global.SOMETHING_WENT_WRONG, 500);
+        else {
+            res.end(global.TOKEN_REQUIRED, 400)
         }
     }
-    else {
-        res.end(global.TOKEN_REQUIRED, 400)
+    catch (err) {
+        res.end(global.SOMETHING_WENT_WRONG, 500);
     }
 }
 
